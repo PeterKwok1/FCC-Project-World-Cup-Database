c3e66ac39a1a343ef98015733b3639bf83ed2e96
Peter Kwok <peter.kwok94@gmail.com>
2024-01-17 00:17:55 -0500

    create tables

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# To Do
-Create db
-  CREATE TABLE games (year date, round varchar(30), winner varchar(30), opponent varchar(30), winner_goals int, opponent_goals int);  
-Insert data
-Query data
-
 # Dump db
 pg_dump -cC --inserts -U freecodecamp worldcup > worldcup.sql
 
--- insert_data.sh
+++ insert_data.sh
@@ -9,16 +9,26 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
+# complete tests 
 # truncate columns
 # read column variables
 # insert column variables
+
+# CREATE DATABASE worldcup
+# \c worldcup
+# CREATE TABLE teams(team_id SERIAL PRIMARY KEY, name VARCHAR(30) UNIQUE NOT NULL);
+# CREATE TABLE games(game_id SERIAL PRIMARY KEY, year INT NOT NULL, round VARCHAR(30) NOT NULL, winner_id INT NOT NULL, opponent_id INT NOT NULL, winner_goals INT NOT NULL, opponent_goals INT NOT NULL);
+
+
+echo $($PSQL "TRUNCATE games")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
   if [[ $YEAR != "year" ]]
   then
-    # INSERT_RESULT=$(PSQL "INSERT INTO games(year, round, winner, opponent, winner_goals, opponent_goals) VALUES('$YEAR-01-01', '$ROUND', '$WINNER', '$OPPONENT', $WINNER_GOALS, $OPPONENT_GOALS)")
-    # echo $INSERT_RESULT
+    INSERT_RESULT=$($PSQL "INSERT INTO games(year, round, winner, opponent, winner_goals, opponent_goals) VALUES('$YEAR-01-01', '$ROUND', '$WINNER', '$OPPONENT', $WINNER_GOALS, $OPPONENT_GOALS)")
+    echo $INSERT_RESULT
+    # echo "$YEAR" $ROUND $WINNER $OPPONENT $WINNER_GOALS $OPPONENT_GOALS
     # Not working for some reason, but the command does. Inspect variables. 
   fi
 done
